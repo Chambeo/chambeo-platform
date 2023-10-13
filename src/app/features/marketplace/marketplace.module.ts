70bd8d1b9f0b2eb9d1af95db72bbc9605dd17b14
Gonzalo Gauna <gonzalogauna2020@gmail.com>
2023-10-13 16:01:19 -0300

    [ADD] Folder structure

---
+++ src/app/features/marketplace/marketplace.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MarketplaceComponent } from './marketplace/marketplace.component';
+
+
+
+@NgModule({
+  declarations: [
+    MarketplaceComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class MarketplaceModule { }
