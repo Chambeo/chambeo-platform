2d2a612e94662f4ad1934ae204679e4c9a13c072
Gonzalo Gauna <gonzalogauna2020@gmail.com>
2023-10-14 18:45:32 -0300

    [FIX] link logo to marketplace route

--- src/app/shared/components/header/header.module.ts
+++ src/app/shared/components/header/header.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header.component';
 import { MaterialModule } from 'src/app/core/material/material.module';
 import { DropdownModule } from '../dropdown/dropdown.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [HeaderComponent],
   imports: [
     CommonModule,
     MaterialModule,
-    DropdownModule
+    DropdownModule,
+    RouterModule
   ],
   exports: [HeaderComponent]
 })
